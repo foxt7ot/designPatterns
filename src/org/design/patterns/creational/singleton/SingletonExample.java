5144053ffbfb0dc2a5c6609dcd91266b859ed821
foxt7ot <myaseen.khan@hotmail.com>
2019-07-10 23:47:12 +0530

    ExampleAdded for singleton

--- src/org/design/patterns/creational/singleton/SingletonExample.java
+++ src/org/design/patterns/creational/singleton/SingletonExample.java
@@ -11,6 +11,19 @@ package org.design.patterns.creational.singleton;
  * You’ll create an instance of the class, if you did not create any such instance earlier; otherwise, you’ll simply reuse the existing one.
  * To take care of thread safety, I use the “synchronized” keyword.
  */
+
+/**
+ * Real World Analogy
+ * Let’s assume that you are a member of a sports team, and your team is participating in a tournament. Your team needs
+ * to play against multiple opponents throughout the tournament. Before each of these matches, as per the rules of the
+ * game, the captains of the two sides must do a coin toss. If your team does not have a captain, you need to elect
+ * someone as a captain. Prior to each game and each coin toss, you may not repeat the process of electing a captain
+ * if you already nominated a person as a captain for the team. Basically, from every team member’s perspective,
+ * there should be only one captain of the team.
+ *
+ * Another example would be of Government before initiation of new phase a government is elected by people which
+ * continues to run until next election
+ */
 //Bill Pugh’s Solution
 class CricketCaptain {
 
